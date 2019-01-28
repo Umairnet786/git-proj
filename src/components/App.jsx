7be7497abc78309b2a096a0ca938aefd053a9a30
umair <umairjutt1548@gmail.com>
2019-01-27 23:56:27 -0800

    umair

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { components } from "react";
+
+class App extends component {
+
+    render(){
+        return(
+
+    <div>
+        <nav>
+            Gameking
+        </nav>
+
+        <header>
+            <div className="log">local call</div>
+        </header>
+
+        <about>
+
+        </about>
+
+        <footer>
+            
+        </footer>
+
+
+
+
+    </div>
+
+        );
+          
+    }
+
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// let a = 9;
+// console.log(a);
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App/>, document.getElementById("app"));
